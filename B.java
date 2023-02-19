44b081183cbe989d3b61f641e259da07eee282af
Vansh Narang <102366482+Vansh-Narang@users.noreply.github.com>
2023-02-19 22:12:29 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,13 @@
+package mypack;
+
+import pack.*;
+
+class B {
+    public static void main(String[] args) {
+        // Creating an object of type class A
+        A a = new A();
+
+        // accessing the method m1()
+        a.display();
+    }
+}
